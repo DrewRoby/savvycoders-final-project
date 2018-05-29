f8cc4cc85c9da59d2191b8469fb0c919f920ae3c
Drew <drew.roby@gmail.com>
2018-05-29 15:25:26 -0500

    Draw reactor components

--- scripts/reactor.js
+++ scripts/reactor.js
@@ -1,103 +1,163 @@
-var canvas = document.getElementById('canvas')
+/* eslint-ignore complexity*/
+var sstgShape = document.getElementById( "sstg" );
+var coreShape = document.getElementById( "core" );
+var steamGeneratorShape = document.getElementById( "steamGenerator" );
+var condenserShape = document.getElementById( "condenser" );
 
 var core = {
-  tIn: 75,
-  tOut: 75,
-  tFuel: 75,
-  maxTemp: 5000,
-  pressure: 0,
-  rodHeight: 0.0,
-  shimSpeed: 1.0, //Do I need this?
-  currentPower: 0
-}
+    "tIn": 75,
+    "tOut": 75,
+    "tFuel": 75,
+    "maxTemp": 5000,
+    "pressure": 0,
+    "rodHeight": 0.0,
+    "shimSpeed": 1.0, // Do I need this?
+    "currentPower": 0
+};
 
 var pressurizer = {
-  heatersOn: false,
-  sprayOn: false
-}
+    "heatersOn": false,
+    "sprayOn": false
+};
 
 var pumps = {
-  rxCoolant: {
-    availableSpeed: ["idle","run"],
-    currentSpeed: "idle"
-  },
-  feed: {
-    availableSpeed: ["idle","run"],
-    currentSpeed: "idle"
-  },
-  condensate: {
-    availableSpeed: ["idle","run"],
-    currentSpeed: "idle"
-  }
-}
+    "rxCoolant": {
+        "availableSpeed": [ "idle","run" ],
+        "currentSpeed": "idle"
+    },
+    "feed": {
+        "availableSpeed": [ "idle","run" ],
+        "currentSpeed": "idle"
+    },
+    "condensate": {
+        "availableSpeed": [ "idle","run" ],
+        "currentSpeed": "idle"
+    }
+};
 
 var valves = {
-  pressurizerSpray: {
-    isOpen: false
-  },
+    "pressurizerSpray": {
+        "isOpen": false
+    },
 
-  feed: {
-    percentOpen: 0
-  },
-  mainEngineThrottle: {
-    percentOpen: 0
-  },
-  tgThrottle: {
-    percentOpen: 0
-  }
-}
+    "feed": {
+        "percentOpen": 0
+    },
+    "mainEngineThrottle": {
+        "percentOpen": 0
+    },
+    "tgThrottle": {
+        "percentOpen": 0
+    }
+};
 
 var steamGenerator = {
-  waterLevel: 75, //in percent of total
-  temp: 75,
-  pressure: 0,
-}
+    "waterLevel": 75, // in percent of total
+    "temp": 75,
+    "pressure": 0,
+};
 
 var turbineGenerator = {
-  percentSteamDemand: 0,
-  maxSteamDemand: 20,
-  rpmSpeed: 0,
-  maxSpeed: 100,
-}
+    "percentSteamDemand": 0,
+    "maxSteamDemand": 20,
+    "rpmSpeed": 0,
+    "maxSpeed": 100,
+};
 
 var mainEngine = {
-  percentSteamDemand: 0,
-  maxSteamDemand: 80
-}
+    "percentSteamDemand": 0,
+    "maxSteamDemand": 80
+};
 
 var condenser = {
-  waterLevel: 50 //in percent of total
-}
+    "waterLevel": 50 // in percent of total
+};
 
-var rxCoolantSwitch = document.querySelector('#rxCoolantSwitch');
-var feedSwitch = document.querySelector('#feedSwitch');
-var condensateSwitch = document.querySelector('#condensateSwitch');
+var rxCoolantSwitch = document.querySelector( "#rxCoolantSwitch" );
+var feedSwitch = document.querySelector( "#feedSwitch" );
+var condensateSwitch = document.querySelector( "#condensateSwitch" );
+
+function draw(){
+    if( sstgShape.getContext ){
+        let ctx = sstgShape.getContext( "2d" );
+
+        ctx.beginPath();
+        ctx.moveTo( 0, 33 );
+        ctx.lineTo( 100, 0 );
+        ctx.lineTo( 100, 100 );
+        ctx.lineTo( 0, 67 );
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    if( coreShape.getContext ){
+        let ctx = coreShape.getContext( "2d" );
+
+        ctx.beginPath();
+        ctx.moveTo( 0, 10 );
+        ctx.lineTo( 0, 140 );
+        ctx.quadraticCurveTo( 50,200, 100,140 );
+        ctx.lineTo( 100,10 );
+        ctx.lineTo( 90,10 );
+        ctx.lineTo( 90,0 );
+        ctx.lineTo( 10,0 );
+        ctx.lineTo( 10,10 );
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    if( steamGeneratorShape.getContext ){
+        let ctx = steamGeneratorShape.getContext( "2d" );
+
+        ctx.beginPath();
+        ctx.moveTo( 150,50 );
+        ctx.arc( 100,50,50,0,Math.PI, true );
+        ctx.lineTo( 50,100 );
+        ctx.arc( 50,150,50,Math.PI * 1.5,Math.PI / 2,true );
+        ctx.lineTo( 150, 200 );
+        ctx.arc( 150,150,50,Math.PI / 2,Math.PI * 1.5,true );
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    if( condenserShape.getContext ){
+        let ctx = condenserShape.getContext( "2d" );
+
+        ctx.beginPath();
+        ctx.moveTo( 50,0 );
+        ctx.quadraticCurveTo( -50, 100, 50, 200 );
+        ctx.lineTo( 250, 200 );
+        ctx.quadraticCurveTo( 350, 100, 250, 0 );
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
 
 // Could I add a shim function to a shimSwitch element that does shimOut/shimIn in a single function?
-function shimOut() = {
-    document.querySelector("#shimSwitch").addEventListener
-    //increment core.rodHeight
-}
+// function shimOut(){
+//     document.querySelector("#shimSwitch").addEventListener
+//     //increment core.rodHeight
+// }
+//
+// function shimIn() {
+//   document.querySelector("#shimSwitch").addEventListener
+//   //decrement core.rodHeight
+// }
+//
+// function changePumpSpeed( pumpID ){
+//   document.querySelector(pumpID){
+//     if(pumps.${pumpID}.currentSpeed==="idle"){
+//       pumps.${pumpID}.currentSpeed = "run";
+//     }else {
+//       pumps.${pumpID}.currentSpeed = "idle"
+//     }
+//   }
+// }
+//
+// function adjustValve( input ){
+//
+// }
 
-function shimIn() = {
-  document.querySelector("#shimSwitch").addEventListener
-  //decrement core.rodHeight
-}
-
-function changePumpSpeed( pumpID ){
-  document.querySelector(pumpID){
-    if(pumps.${pumpID}.currentSpeed==="idle"){
-      pumps.${pumpID}.currentSpeed = "run";
-    }else {
-      pumps.${pumpID}.currentSpeed = "idle"
-    }
-  }
-}
-
-function adjustValve( input ){
-
-}
-
-rxCoolantSwitch.addEventListener("onclick",changePumpSpeed("rxCoolant"));
-feedSwitch.addEventListener("onclick",changePumpSpeed("feed"));
-condensateSwitch.addEventListener("onclick",changePumpSpeed("condensate"));
+rxCoolantSwitch.addEventListener( "onclick",changePumpSpeed( "rxCoolant" ) );
+feedSwitch.addEventListener( "onclick",changePumpSpeed( "feed" ) );
+condensateSwitch.addEventListener( "onclick",changePumpSpeed( "condensate" ) );
